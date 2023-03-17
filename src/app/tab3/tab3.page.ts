272bc8a363a7e456b04c6f795d6ea01b13cee986
João Vitor Zamarin <joao.zamarin@gmail.com>
2023-03-17 20:35:19 -0300

    jogo dado

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  tamanho = 6;
+  valorAtual = 0;
+  ativo = false;
+
+  imgDados = [
+    'assets/dice-red.png',
+    'assets/dice.gif'
+  ];
+  imagemDado = this.imgDados[0];
+
   constructor() {}
+
+  jogarDado() {
+    this.imagemDado = this.imgDados[1];
+    this.ativo = true;
+
+    setTimeout(() => {
+      this.valorAtual = Math.floor(Math.random() * this.tamanho) + 1;
+      this.ativo = false;
+    }, 2000);
+
+    this.valorAtual = 0;
+  }
 
 }
